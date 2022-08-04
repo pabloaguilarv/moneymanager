322b8fe1711c3988c3ebcc99356fb0c7a5afc01d
Pablo Aguilar <baster94@hotmail.com>
2022-08-04 18:03:49 -0500

    forgot this

--- src/moneymanager/settings.py
+++ src/moneymanager/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-45_-u!v^l=fz9__3sg3%5wed!67uo9i_z36n35+m_m(h^%r7y8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
